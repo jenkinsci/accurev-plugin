b59af2e4c280d7d601f2e9688071ed3f2ee259c3
statlor <statlor@dev.java.net>
2009-07-27 13:40:03 +0000

    [HUDSON 3967] Code to hopefully fix this issue, making it available for review.

--- src/main/java/hudson/plugins/accurev/AccurevSCM.java
+++ src/main/java/hudson/plugins/accurev/AccurevSCM.java
@@ -833,6 +833,10 @@ public class AccurevSCM extends SCM {
                 if (!"transaction".equalsIgnoreCase(parser.getName())) {
                     continue;
                 }
+                // ignore "dispatch" transactions - these are AccuWork
+                if ("dispatch".equalsIgnoreCase(parser.getAttributeValue(null, "type"))) {
+                    continue;
+                }
                 break;
             }
             String transactionId = parser.getAttributeValue("", "id");
