1fe3266230205cfbb9c7b9d4c3b9c6dca3ed3887
JSherwood <jsherwood@accurev.com>
2014-07-23 16:14:32 -0400

    Updated to include changes from pull req 12

--- src/main/java/hudson/plugins/accurev/AccuRevRefTreeProcessor.java
+++ src/main/java/hudson/plugins/accurev/AccuRevRefTreeProcessor.java
@@ -220,14 +220,11 @@ public class AccuRevRefTreeProcessor {
                   
                    if(scm.isCleanreftree()){
                       final Map<String, RefTreeExternalFile> externalFiles = getReftreesStatus(server, accurevEnv, accurevWorkingSpace, listener, accurevClientExePath, launcher);
-                      File toBeDeleted;
-                      for(int i=0; i<externalFiles.size(); i++)
-                      {
-                         RefTreeExternalFile extFileObject = externalFiles.get(i);
-                         listener.getLogger().println("Update failed -- External File path -"+extFileObject.getLocation());
-                         toBeDeleted= new File(extFileObject.getLocation());
-                         if(toBeDeleted.exists())
-                         {
+                      for(RefTreeExternalFile extFileObject : externalFiles.values()) {
+                         String fileLocation = extFileObject.getLocation();
+                         listener.getLogger().println("Update failed -- External File path - " + fileLocation);
+                         File toBeDeleted = new File(fileLocation);
+                         if(toBeDeleted.exists()) {
                             toBeDeleted.delete();
                          }
                       }                                                     
