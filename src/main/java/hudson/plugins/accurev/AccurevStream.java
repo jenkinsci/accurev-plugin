jenkinsci/accurev-plugin

commit 53d62927bc9a0a07a3c2a286ab3b3d1e89a183b5

Code cleanup, Jenkins-4369 FIX, Log spamming, Javadoc (#25)
* ignore intellij

* fixed failing test

Test needed jelly escape by default set to true

* Override not allowed on interface method

Override is not allowed when implementing interface method

* JENKINS-4369 Fix - Workspace relocation check false positivities

https://github.com/jenkinsci/accurev-plugin/pull/11

* Spamming log

* Uncessary cast

* Unused import

* Redundant modifier

* Unnecessary boxing

* for loop instead of while.

* Redundant modifier

* First stab at updating to 2.7 LTS

Still need a few more deprecation warning to go away.

* Fix unchecked

* Try with Resource

* Collapsed catch

* Forgot to remove redundant cast

* Diamond types

* Code cleanup and unused imports

* Proper comment

* First fix to JavaDoc

* Javadoc only

* Findbugs: Garbage collection call it bad

* FindBug: Method call passes null for non-null parameter

* FindBugs: Reliance on default encoding

* more idea ignore

* All FindBugs, fixed.

* Update .gitignore

* Target java 1.8

* FindBugs null pointer

Possible null pointer dereference due to return value of called method
Also optimise import

* Some more and some less null pointer

Please pass already :smile:

* FindBug dance :tada:

* deprecation warnings cleared, some more code cleanup.

Tested on Jenkins 2.7

* Bumping to 2.13 jenkins plugin

Last touch :tada:

* Last one, installation path on macOS :apple:

MacOS default installation with the anywhere installer is now on
Applications/AccuRev.

* Unused dependency?

* unused import

* bump plugin versions

* update maintainer

* prepare for 0.7.0 release

src/main/java/hudson/plugins/accurev/AccurevStream.java
@@ -1,5 +1,7 @@
 package hudson.plugins.accurev;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
@@ -22,7 +24,8 @@ public class AccurevStream implements Serializable {
     private final Date time;
     private final Date startTime;
     private transient AccurevStream parent;
-    private final transient Set<AccurevStream> children = new HashSet<AccurevStream>();
+    private transient Set<AccurevStream> children = new HashSet<>();
+    private static final long serialVersionUID = 8004696899509026973L;
 
     public AccurevStream(String name, Long number, String depot, String basisName, Long basisNumber, boolean dynamic, StreamType type, Date time, Date startTime) {
         this.name = name;
@@ -32,8 +35,14 @@ public class AccurevStream implements Serializable {
         this.basisNumber = basisNumber;
         this.dynamic = dynamic;
         this.type = type;
-        this.time = time;
-        this.startTime = startTime;
+        this.time = (Date) time.clone();
+        this.startTime = (Date) startTime.clone();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+
+        children = new HashSet<>();
     }
 
     /**
@@ -122,7 +131,7 @@ public class AccurevStream implements Serializable {
      * @return Value for property 'time'.
      */
     public Date getTime() {
-        return time;
+        return (Date) time.clone();
     }
 
     /**
@@ -131,7 +140,7 @@ public class AccurevStream implements Serializable {
      * @return Value for property 'startTime'.
      */
     public Date getStartTime() {
-        return startTime;
+        return (Date) startTime.clone();
     }
 
     /**
@@ -178,7 +187,7 @@ public class AccurevStream implements Serializable {
         }
     }
 
-    public static enum StreamType {
+    public enum StreamType {
         NORMAL("normal"),
         SNAPSHOT("snapshot"),
         WORKSPACE("workspace"),

src/main/java/hudson/plugins/accurev/cmd/ShowStreams.java
@@ -11,9 +11,8 @@ import hudson.plugins.accurev.AccurevSCM.AccurevServer;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.nio.charset.Charset;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.logging.Level;
@@ -90,7 +89,7 @@ public class ShowStreams extends Command {
          final TaskListener listener, //
          final String accurevPath, //
          final Launcher launcher) {
-      final Map<String, AccurevStream> streams = new HashMap<String, AccurevStream>();
+      final Map<String, AccurevStream> streams = new HashMap<>();
       String streamName = nameOfStreamRequired;
       while (streamName != null && !streamName.isEmpty()) {
          final Map<String, AccurevStream> oneStream = getOneStream(scm, streamName, server, accurevEnv, workspace, listener, accurevPath, launcher);
@@ -143,7 +142,6 @@ public class ShowStreams extends Command {
 	         ) {
 		
         final ArgumentListBuilder cmd = new ArgumentListBuilder();
-		List<String> showStreamsCmd = new ArrayList<String>();
 		  
 		cmd.add(accurevPath);
         cmd.add("show");
@@ -156,9 +154,8 @@ public class ShowStreams extends Command {
         if(depot==null || depot.equalsIgnoreCase("")){
       	  return cbm;
         }
-        showStreamsCmd = cmd.toList();
         
-        ProcessBuilder processBuilder = new ProcessBuilder(showStreamsCmd);
+        ProcessBuilder processBuilder = new ProcessBuilder(cmd.toList());
         processBuilder.redirectErrorStream(true);
         InputStream stdout = null;
         
@@ -174,7 +171,7 @@ public class ShowStreams extends Command {
 	           DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	           DocumentBuilder parser;
 	           parser = factory.newDocumentBuilder();
-	           Document doc = parser.parse(new ByteArrayInputStream(showcmdoutputdata.getBytes()));
+	           Document doc = parser.parse(new ByteArrayInputStream(showcmdoutputdata.getBytes(Charset.defaultCharset())));
 	           doc.getDocumentElement().normalize();
 	           NodeList nList = doc.getElementsByTagName("stream");
 	           for (int i = 0; i < nList.getLength(); i++) {
@@ -191,15 +188,9 @@ public class ShowStreams extends Command {
 	        } else {
 	           descriptorlogger.warning("AccuRev Server: "+server.getName()+". "+ showcmdoutputdata);
 	        }
-		} catch (IOException e) {
+		} catch (IOException | SAXException | ParserConfigurationException | InterruptedException e) {
 	         descriptorlogger.log(Level.WARNING, "AccuRev Server: "+server.getName()+". "+"Could not populate stream list.", e.getCause());
-	      } catch (InterruptedException e) {
-	         descriptorlogger.log(Level.WARNING, "AccuRev Server: "+server.getName()+". "+"Could not populate stream list.", e.getCause());
-	      } catch (ParserConfigurationException e) {
-	         descriptorlogger.log(Level.WARNING, "AccuRev Server: "+server.getName()+". "+"Could not populate stream list.", e.getCause());
-	      } catch (SAXException e) {
-	         descriptorlogger.log(Level.WARNING, "AccuRev Server: "+server.getName()+". "+"Could not populate stream list.", e.getCause());
-	      }finally {
+	      } finally {
 	          try {
 	         	 if(stdout!=null){
 	         		 stdout.close();

src/main/java/hudson/plugins/accurev/parsers/xml/ParseShowStreams.java
@@ -22,18 +22,17 @@ public final class ParseShowStreams implements ICmdOutputXmlParser<Map<String, A
     public Map<String, AccurevStream> parse(XmlPullParser parser, String depot) throws UnhandledAccurevCommandOutput,
             IOException, XmlPullParserException {
     	
-        final Map<String, AccurevStream> streams = new HashMap<String, AccurevStream>();
+        final Map<String, AccurevStream> streams = new HashMap<>();
         while (true) {
             switch (parser.next()) {
             case XmlPullParser.START_DOCUMENT:
                 break;
             case XmlPullParser.END_DOCUMENT:
                 // build the tree
-                for (AccurevStream stream : streams.values()) {
-                    if (stream.getBasisName() != null) {
-                        stream.setParent(streams.get(stream.getBasisName()));
-                    }
-                }
+                streams.values()
+                        .stream()
+                        .filter(stream -> stream.getBasisName() != null)
+                        .forEachOrdered(stream -> stream.setParent(streams.get(stream.getBasisName())));
                 return streams;
             case XmlPullParser.START_TAG:
                 final String tagName = parser.getName();
@@ -47,9 +46,9 @@ public final class ParseShowStreams implements ICmdOutputXmlParser<Map<String, A
                     final String streamTypeStr = parser.getAttributeValue("", "type");
                     final String streamIsDynamic = parser.getAttributeValue("", "isDynamic");
                     final String streamTimeString = parser.getAttributeValue("", "time");
-                    final Date streamTime = streamTimeString == null ? null : ParseChangeLog.convertAccurevTimestamp(streamTimeString);
+                    final Date streamTime = streamTimeString == null ? new Date(0) : ParseChangeLog.convertAccurevTimestamp(streamTimeString);
                     final String streamStartTimeString = parser.getAttributeValue("", "startTime");
-                    final Date streamStartTime = streamTimeString == null ? null : ParseChangeLog.convertAccurevTimestamp(streamStartTimeString);
+                    final Date streamStartTime = streamTimeString == null ? new Date(0) : ParseChangeLog.convertAccurevTimestamp(streamStartTimeString);
                     try {
                         final Long streamNumber = streamNumberStr == null ? null : Long.valueOf(streamNumberStr);
                         final Long basisStreamNumber = basisStreamNumberStr == null ? null : Long.valueOf(basisStreamNumberStr);

.gitignore
@@ -5,3 +5,5 @@ work
 .classpath
 .project
 .settings
+.idea
+*.iml

pom.xml
@@ -1,79 +1,103 @@
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
     <modelVersion>4.0.0</modelVersion>
 
     <parent>
         <groupId>org.jenkins-ci.plugins</groupId>
         <artifactId>plugin</artifactId>
-        <version>1.525</version>
+        <version>2.13</version>
         <relativePath>../pom.xml</relativePath>
     </parent>
 
     <artifactId>accurev</artifactId>
     <packaging>hpi</packaging>
-    <version>0.6.36-SNAPSHOT</version>
+    <version>0.7.0-SNAPSHOT</version>
     <name>Jenkins Accurev plugin</name>
     <url>http://wiki.jenkins-ci.org/display/JENKINS/AccuRev+Plugin</url>
 
     <developers>
-      <developer>
-        <id>statler</id>
-        <name>Scott Tatum</name>
-      </developer>
-      <developer>
-        <id>helterscelter</id>
-	     <name>Clint Martin</name>
-      </developer>
+        <developer>
+            <id>casz</id>
+            <name>Joseph Petersen</name>
+        </developer>
     </developers>
-    
-    <dependencies>
-      <dependency>
-        <groupId>org.codehaus.plexus</groupId>
-        <artifactId>plexus-utils</artifactId>
-        <version>1.5.1</version>
-      </dependency>
-    </dependencies>
 
     <scm>
-      <connection>scm:git:git://github.com/jenkinsci/accurev-plugin.git</connection>
-      <developerConnection>scm:git:git@github.com:jenkinsci/accurev-plugin.git</developerConnection>
-      <url>https://github.com/jenkinsci/accurev-plugin</url>
+        <connection>scm:git:git://github.com/jenkinsci/accurev-plugin.git</connection>
+        <developerConnection>scm:git:git@github.com:jenkinsci/accurev-plugin.git</developerConnection>
+        <url>https://github.com/jenkinsci/accurev-plugin</url>
     </scm>
 
     <distributionManagement>
         <repository>
-          <id>maven.jenkins-ci.org</id>
-          <url>http://maven.jenkins-ci.org:8081/content/repositories/releases</url>
+            <uniqueVersion>false</uniqueVersion>
+            <id>maven.jenkins-ci.org</id>
+            <url>https://repo.jenkins-ci.org/releases/</url>
         </repository>
+        <snapshotRepository>
+            <id>maven.jenkins-ci.org</id>
+            <url>https://repo.jenkins-ci.org/snapshots/</url>
+        </snapshotRepository>
     </distributionManagement>
 
     <build>
-	<plugins>
-		<plugin>
-			<groupId>org.apache.maven.plugins</groupId>
-			<artifactId>maven-release-plugin</artifactId>
-			<version>2.0</version>
-			<dependencies>
-				<dependency>
-					<groupId>org.apache.maven.scm</groupId>
-					<artifactId>maven-scm-provider-gitexe</artifactId>
-					<version>1.3</version>
-				</dependency>
-			</dependencies>
-			<configuration>
-				<goals>deploy</goals>
-			</configuration>
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-release-plugin</artifactId>
+                <version>2.5.3</version>
+                <dependencies>
+                    <dependency>
+                        <groupId>org.apache.maven.scm</groupId>
+                        <artifactId>maven-scm-provider-gitexe</artifactId>
+                        <version>1.9.5</version>
+                    </dependency>
+                </dependencies>
+                <configuration>
+                    <goals>deploy</goals>
+                </configuration>
 
-		</plugin>
-	</plugins>
+            </plugin>
+            <plugin>
+                <groupId>org.codehaus.mojo</groupId>
+                <artifactId>animal-sniffer-maven-plugin</artifactId>
+                <version>1.15</version>
+                <configuration>
+                    <signature>
+                        <groupId>org.codehaus.mojo.signature</groupId>
+                        <artifactId>java18</artifactId>
+                        <version>1.0</version>
+                    </signature>
+                </configuration>
+                <executions>
+                    <execution>
+                        <id>ensure-java-1.8-class-library</id>
+                        <phase>test</phase>
+                        <goals>
+                            <goal>check</goal>
+                        </goals>
+                    </execution>
+                </executions>
+            </plugin>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-compiler-plugin</artifactId>
+                <version>3.5.1</version>
+                <configuration>
+                    <source>1.8</source>
+                    <target>1.8</target>
+                </configuration>
+            </plugin>
+        </plugins>
     </build>
 
-  <licenses>
-    <license>
-      <name>The MIT license</name>
-      <url>http://www.opensource.org/licenses/mit-license.php</url>
-      <distribution>repo</distribution>
-    </license>
-  </licenses>
+    <licenses>
+        <license>
+            <name>The MIT license</name>
+            <url>http://www.opensource.org/licenses/mit-license.php</url>
+            <distribution>repo</distribution>
+        </license>
+    </licenses>
 
     <repositories>
         <repository>

src/main/java/hudson/plugins/accurev/AccurevChangeLogSet.java
@@ -1,6 +1,6 @@
 package hudson.plugins.accurev;
 
-import hudson.model.AbstractBuild;
+import hudson.model.Run;
 import hudson.scm.ChangeLogSet;
 
 import java.util.Collection;
@@ -21,13 +21,14 @@ import org.kohsuke.stapler.export.ExportedBean;
 public final class AccurevChangeLogSet extends ChangeLogSet<AccurevTransaction> {
     private final List<AccurevTransaction> transactions;
 
-    AccurevChangeLogSet(AbstractBuild build, List<AccurevTransaction> transactions) {
-        super(build);
+    AccurevChangeLogSet(Run build, List<AccurevTransaction> transactions) {
+        // TODO: Implement RepositoryBrowser?
+        super(build, null);
         if (transactions == null) {
             throw new NullPointerException("Cannot have a null transaction list");
         }
         this.transactions = Collections.unmodifiableList(transactions);
-        for (AccurevTransaction transaction : transactions) {
+        for (AccurevTransaction transaction: transactions) {
             transaction.setParent(this);
         }
     }

commit c2f349add279fd45cd87ad4274cec760a5f83078

[JENKINS-28118] Fixed poll on ancestor
Poll failed searching in parents when “Show one stream at a time” was
checked.
Fixed regression in PR #25

src/main/java/hudson/plugins/accurev/AccurevStream.java
@@ -35,8 +35,8 @@ public class AccurevStream implements Serializable {
         this.basisNumber = basisNumber;
         this.dynamic = dynamic;
         this.type = type;
-        this.time = (Date) time.clone();
-        this.startTime = (Date) startTime.clone();
+        this.time = time == null ? null : (Date) time.clone();
+        this.startTime = startTime == null ? null :  (Date) startTime.clone();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {

src/main/java/hudson/plugins/accurev/cmd/ShowStreams.java
@@ -77,7 +77,7 @@ public class ShowStreams extends Command {
       cmd.add("streams");
       final Map<String, AccurevStream> streams = AccurevLauncher.runCommand("Show streams command", launcher, cmd, null, scm.getOptionalLock(), accurevEnv,
             workspace, listener, logger, XmlParserFactory.getFactory(), new ParseShowStreams(), scm.getDepot());
-      return streams;
+      return setParents(streams);
    }
 
    private static Map<String, AccurevStream> getAllAncestorStreams(//
@@ -104,7 +104,7 @@ public class ShowStreams extends Command {
             streams.putAll(oneStream);
          }
       }
-      return streams;
+      return setParents(streams);
    }
 
    private static Map<String, AccurevStream> getOneStream(//
@@ -130,6 +130,15 @@ public class ShowStreams extends Command {
             accurevEnv, workspace, listener, logger, XmlParserFactory.getFactory(), new ParseShowStreams(), scm.getDepot());
       return oneStream;
    }
+
+    private static Map<String, AccurevStream> setParents(Map<String, AccurevStream> streams) {
+        //build the tree
+        streams.values()
+                .stream()
+                .filter(stream -> stream.getBasisName() != null)
+                .forEachOrdered(stream -> stream.setParent(streams.get(stream.getBasisName())));
+        return streams;
+    }
    
    //Populating streams dynamically in the global config page
    

src/main/java/hudson/plugins/accurev/parsers/xml/ParseShowStreams.java
@@ -28,11 +28,6 @@ public final class ParseShowStreams implements ICmdOutputXmlParser<Map<String, A
             case XmlPullParser.START_DOCUMENT:
                 break;
             case XmlPullParser.END_DOCUMENT:
-                // build the tree
-                streams.values()
-                        .stream()
-                        .filter(stream -> stream.getBasisName() != null)
-                        .forEachOrdered(stream -> stream.setParent(streams.get(stream.getBasisName())));
                 return streams;
             case XmlPullParser.START_TAG:
                 final String tagName = parser.getName();
@@ -46,9 +41,9 @@ public final class ParseShowStreams implements ICmdOutputXmlParser<Map<String, A
                     final String streamTypeStr = parser.getAttributeValue("", "type");
                     final String streamIsDynamic = parser.getAttributeValue("", "isDynamic");
                     final String streamTimeString = parser.getAttributeValue("", "time");
-                    final Date streamTime = streamTimeString == null ? new Date(0) : ParseChangeLog.convertAccurevTimestamp(streamTimeString);
+                    final Date streamTime = streamTimeString == null ? null : ParseChangeLog.convertAccurevTimestamp(streamTimeString);
                     final String streamStartTimeString = parser.getAttributeValue("", "startTime");
-                    final Date streamStartTime = streamTimeString == null ? new Date(0) : ParseChangeLog.convertAccurevTimestamp(streamStartTimeString);
+                    final Date streamStartTime = streamTimeString == null ? null : ParseChangeLog.convertAccurevTimestamp(streamStartTimeString);
                     try {
                         final Long streamNumber = streamNumberStr == null ? null : Long.valueOf(streamNumberStr);
                         final Long basisStreamNumber = basisStreamNumberStr == null ? null : Long.valueOf(basisStreamNumberStr);