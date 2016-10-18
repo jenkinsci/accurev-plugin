3292429cffbe7280220c189f164305e7126f39cc
Joseph <casz@users.noreply.github.com>
2016-10-18 09:13:26 +0200

    Import EOFException :tada:

--- src/main/java/hudson/plugins/accurev/parsers/xml/ParseGetConfig.java
+++ src/main/java/hudson/plugins/accurev/parsers/xml/ParseGetConfig.java
@@ -6,6 +6,7 @@ import hudson.plugins.accurev.GetConfigWebURL;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
