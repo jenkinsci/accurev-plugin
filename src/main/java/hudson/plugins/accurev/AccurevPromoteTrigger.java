c0b14837957903c8bf6b171b4c689247af25386b
Joseph Petersen <josephp90@gmail.com>
2017-03-22 12:14:10 +0100

    Fix PromoteTrigger to work with Pipeline

--- src/main/java/hudson/plugins/accurev/AccurevPromoteTrigger.java
+++ src/main/java/hudson/plugins/accurev/AccurevPromoteTrigger.java
@@ -7,13 +7,16 @@ import hudson.model.*;
 import hudson.model.listeners.ItemListener;
 import hudson.plugins.accurev.AccurevSCM.AccurevSCMDescriptor;
 import hudson.plugins.accurev.AccurevSCM.AccurevServer;
+import hudson.scm.SCM;
 import hudson.triggers.Trigger;
 import hudson.triggers.TriggerDescriptor;
 import hudson.util.StreamTaskListener;
 import jenkins.model.Jenkins;
+import jenkins.model.ParameterizedJobMixIn;
 import org.apache.commons.jelly.XMLOutput;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.CheckForNull;
@@ -21,6 +24,7 @@ import javax.annotation.Nonnull;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -28,7 +32,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 /**
  * Initialized by josp on 16/08/16.
  */
-public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
+public class AccurevPromoteTrigger extends Trigger<Job> {
 
     private static final Logger LOGGER = Logger.getLogger(AccurevPromoteTrigger.class.getName());
     private static final HashMap<String, AccurevPromoteListener> listeners = new HashMap<>();
@@ -53,12 +57,14 @@ public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
         for (Project<?, ?> p : Jenkins.getInstance().getAllItems(Project.class)) {
             AccurevPromoteTrigger t = p.getTrigger(AccurevPromoteTrigger.class);
             if (t != null) {
-                if (t.getServer().isUsePromoteListen()) {
-                    String host = t.getServer().getHost();
-                    if (StringUtils.isNotEmpty(host)) {
-                        AccurevPromoteListener listener = listeners.get(host);
-                        if (null != listener) {
-                            listener.addTrigger(t);
+                for (AccurevSCM scm : t.getSCMs()) {
+                    if (scm.getServer().isUsePromoteListen()) {
+                        String host = scm.getServer().getHost();
+                        if (StringUtils.isNotEmpty(host)) {
+                            AccurevPromoteListener listener = listeners.get(host);
+                            if (null != listener) {
+                                listener.addTrigger(t);
+                            }
                         }
                     }
                 }
@@ -87,17 +93,23 @@ public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
     }
 
     @Override
-    public void start(AbstractProject<?, ?> project, boolean newInstance) {
+    public void start(Job project, boolean newInstance) {
         super.start(project, newInstance);
-        AccurevServer server = getServer();
-        String host = server.getHost();
-        if (StringUtils.isNotEmpty(host)) {
-            initServer(host, server);
-            AccurevPromoteListener listener = listeners.get(host);
+        for (AccurevSCM scm : getSCMs()) {
+            AccurevServer server = scm.getServer();
+            if (server != null) {
+                String host = server.getHost();
+                if (StringUtils.isNotEmpty(host)) {
+                    initServer(host, server);
+                    AccurevPromoteListener listener = listeners.get(host);
 
-            if (listener != null) {
-                listener.addTrigger(this);
-                removeDuplicatedTriggers(listener.getTriggers());
+                    if (listener != null) {
+                        listener.addTrigger(this);
+                        removeDuplicatedTriggers(listener.getTriggers());
+                    }
+                }
+            } else {
+                LOGGER.log(Level.WARNING, "Server is null");
             }
         }
     }
@@ -113,13 +125,15 @@ public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
 
     @Override
     public void stop() {
-        AccurevServer server = getServer();
-        if (server != null) {
-            String host = server.getHost();
-            if (StringUtils.isNotEmpty(host)) {
-                AccurevPromoteListener listener = listeners.get(host);
-                if (listener != null) {
-                    listener.removeTrigger(this);
+        for (AccurevSCM scm : getSCMs()) {
+            AccurevServer server = scm.getServer();
+            if (server != null) {
+                String host = server.getHost();
+                if (StringUtils.isNotEmpty(host)) {
+                    AccurevPromoteListener listener = listeners.get(host);
+                    if (listener != null) {
+                        listener.removeTrigger(this);
+                    }
                 }
             }
         }
@@ -130,28 +144,17 @@ public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
         return job == null ? "" : job.getName();
     }
 
-    public String getDepot() {
-        AccurevSCM scm = getScm();
-        return scm == null ? "" : scm.getDepot();
-    }
-
-    public String getStream() {
-        AccurevSCM scm = getScm();
-        return scm == null ? "" : scm.getStream();
-    }
-
-    public AccurevServer getServer() {
-        AccurevSCM scm = getScm();
-        if (null != scm) {
-            return scm.getServer();
-        }
-        return null;
-    }
-
     public void scheduleBuild(String author, String stream) {
         LOGGER.fine("schedule build: " + getProjectName());
-        if (job != null) job.scheduleBuild2(10, new AccurevPromoteCause(author, stream));
-        else LOGGER.warning("Job is null");
+        @SuppressWarnings("rawtypes")
+        ParameterizedJobMixIn pJob = new ParameterizedJobMixIn() {
+            @Override
+            protected Job asJob() {
+                return job;
+            }
+        };
+
+        pJob.scheduleBuild(new AccurevPromoteCause(author, stream));
     }
 
     @Override
@@ -160,60 +163,75 @@ public class AccurevPromoteTrigger extends Trigger<AbstractProject<?, ?>> {
     }
 
     @CheckForNull
-    public AccurevSCM getScm() {
-        if (job != null && job.getScm() instanceof AccurevSCM) {
-            return (AccurevSCM) job.getScm();
+    public Collection<AccurevSCM> getSCMs() {
+        Collection<AccurevSCM> scms = new ArrayList<>();
+        if (job != null) {
+            if (job instanceof WorkflowJob) {
+                WorkflowJob wJob = (WorkflowJob) job;
+                for (SCM scm : wJob.getSCMs()) {
+                    if (scm instanceof AccurevSCM) {
+                        scms.add((AccurevSCM) scm);
+                    }
+                }
+            } else {
+                SCM scm = ((AbstractProject) job).getScm();
+                if (scm instanceof AccurevSCM) {
+                    scms.add((AccurevSCM) scm);
+                }
+            }
         }
-        return null;
+        return scms;
     }
 
     public boolean checkForChanges(String promoteDepot, String promoteStream, int promoteTrans, Map<String, AccurevStream> streams) {
         try (StreamTaskListener listener = new StreamTaskListener(getLogFile())) {
             PrintStream logger = listener.getLogger();
-            AccurevSCM scm = getScm();
-            if (scm == null || job == null) return false;
-            if (getStream().equals(promoteStream)) {
-                logger.println("Matching stream: " + promoteStream);
+            Collection<AccurevSCM> scms = getSCMs();
+            if (scms == null || job == null) return false;
+            for (AccurevSCM scm : scms) {
+                if (scm.getStream().equals(promoteStream)) {
+                    logger.println("Matching stream: " + promoteStream);
 
-                int lastTrans = NumberUtils.toInt(getLastTransaction(job), 0);
-                logger.println("Last build Transaction: " + lastTrans + ", promote transaction: " + promoteTrans);
-                if (promoteTrans > lastTrans) {
-                    setLastTransaction(job, String.valueOf(promoteTrans));
-                    return true;
-                }
-            } else if (promoteDepot.equals(getDepot()) && !scm.isIgnoreStreamParent()) {
-
-                String localStream;
-                try {
-                    localStream = scm.getPollingStream(job, listener);
-                } catch (IllegalArgumentException ex) {
-                    listener.getLogger().println(ex.getMessage());
-                    return false;
-                }
-
-                if (streams == null) {
-                    listener.fatalError("streams EMPTY");
-                    return false;
-                }
-                if (!streams.containsKey(localStream)) {
-                    listener.fatalError("The specified stream, '" + localStream + "' does not appear to exist!");
-                    return false;
-                }
-                AccurevStream stream = streams.get(localStream);
-                do {
-                    if (stream.getName().equals(promoteStream)) {
-                        logger.println("Found matching parent stream: " + promoteStream);
-
-                        int lastTrans = NumberUtils.toInt(getLastTransaction(job), 0);
-                        logger.println("Last build Transaction: " + lastTrans + ", promote transaction: " + promoteTrans);
-                        if (promoteTrans > lastTrans) {
-                            setLastTransaction(job, String.valueOf(promoteTrans));
-                            return true;
-                        }
+                    int lastTrans = NumberUtils.toInt(getLastTransaction(job), 0);
+                    logger.println("Last build Transaction: " + lastTrans + ", promote transaction: " + promoteTrans);
+                    if (promoteTrans > lastTrans) {
+                        setLastTransaction(job, String.valueOf(promoteTrans));
+                        return true;
                     }
-                    stream = stream.getParent();
-                } while (stream != null && stream.isReceivingChangesFromParent());
-                logger.println("No matching parent stream found");
+                } else if (promoteDepot.equals(scm.getDepot()) && !scm.isIgnoreStreamParent()) {
+
+                    String localStream;
+                    try {
+                        localStream = scm.getPollingStream(job, listener);
+                    } catch (IllegalArgumentException ex) {
+                        listener.getLogger().println(ex.getMessage());
+                        return false;
+                    }
+
+                    if (streams == null) {
+                        listener.fatalError("streams EMPTY");
+                        return false;
+                    }
+                    if (!streams.containsKey(localStream)) {
+                        listener.fatalError("The specified stream, '" + localStream + "' does not appear to exist!");
+                        return false;
+                    }
+                    AccurevStream stream = streams.get(localStream);
+                    do {
+                        if (stream.getName().equals(promoteStream)) {
+                            logger.println("Found matching parent stream: " + promoteStream);
+
+                            int lastTrans = NumberUtils.toInt(getLastTransaction(job), 0);
+                            logger.println("Last build Transaction: " + lastTrans + ", promote transaction: " + promoteTrans);
+                            if (promoteTrans > lastTrans) {
+                                setLastTransaction(job, String.valueOf(promoteTrans));
+                                return true;
+                            }
+                        }
+                        stream = stream.getParent();
+                    } while (stream != null && stream.isReceivingChangesFromParent());
+                    logger.println("No matching parent stream found");
+                }
             }
         } catch (IllegalArgumentException | IOException ex) {
             LOGGER.warning(ex.getMessage());
